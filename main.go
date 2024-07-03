7a176a154557302630b7cd0e765aab9e8817ed02
Ashish Shinde <ashish.shinde@lemmatechnologies.com>
2024-07-03 15:49:03 +0530

    removed api key

--- main.go
+++ main.go
@@ -40,65 +40,6 @@ func extractCoordinatesFromImage(imagePath string) (float64, float64, error) {
 	return lat, long, nil
 }
 
-// const (
-// 	googleMapsAPIKey = "AIzaSyC2qJatOdSsOK1XY_TcedFOkABy297DOVw"
-// )
-
-// type GeocodingResponse struct {
-// 	Results []GeocodingResult `json:"results"`
-// 	Status  string            `json:"status"`
-// }
-
-// type GeocodingResult struct {
-// 	FormattedAddress  string             `json:"formatted_address"`
-// 	AddressComponents []AddressComponent `json:"address_components"`
-// }
-
-// type AddressComponent struct {
-// 	LongName  string   `json:"long_name"`
-// 	ShortName string   `json:"short_name"`
-// 	Types     []string `json:"types"`
-// }
-
-// func reverseGeocode(latitude, longitude float64) (string, string, error) {
-// 	url := fmt.Sprintf("https://maps.googleapis.com/maps/api/geocode/json?latlng=%f,%f&key=%s", latitude, longitude, googleMapsAPIKey)
-
-// 	resp, err := http.Get(url)
-// 	if err != nil {
-// 		return "", "", err
-// 	}
-// 	defer resp.Body.Close()
-
-// 	if resp.StatusCode != http.StatusOK {
-// 		return "", "", fmt.Errorf("HTTP status code %d", resp.StatusCode)
-// 	}
-
-// 	var geocodingResponse GeocodingResponse
-// 	err = json.NewDecoder(resp.Body).Decode(&geocodingResponse)
-// 	if err != nil {
-// 		return "", "", err
-// 	}
-
-// 	if len(geocodingResponse.Results) > 0 {
-// 		formattedAddress := geocodingResponse.Results[0].FormattedAddress
-// 		pincode := extractPincode(geocodingResponse.Results[0])
-// 		return formattedAddress, pincode, nil
-// 	}
-
-// 	return "", "", fmt.Errorf("no results found")
-// }
-
-//	func extractPincode(result GeocodingResult) string {
-//		for _, addrComponent := range result.AddressComponents {
-//			for _, addrType := range addrComponent.Types {
-//				if addrType == "postal_code" {
-//					return addrComponent.LongName
-//				}
-//			}
-//		}
-//		return ""
-//	}
-
 func reverseGeocode(latitude, longitude float64) (string, error) {
 	url := fmt.Sprintf("https://nominatim.openstreetmap.org/reverse?format=json&lat=%f&lon=%f", latitude, longitude)
 
@@ -131,49 +72,7 @@ func main() {
 
 	fmt.Printf("Latitude: %f, Longitude: %f\n", latitude, longitude)
 
-	// Reverse geocode to get address and pincode
-	// address, pincode, err := reverseGeocode(latitude, longitude)
-	// if err != nil {
-	// 	log.Fatalf("Error reverse geocoding: %v", err)
-	// }
-
 	fmt.Printf("Latitude: %f, Longitude: %f\n", latitude, longitude)
-	// fmt.Printf("Address: %s\n", address)
-	// fmt.Printf("Pincode: %s\n", pincode)
-
-	// apiKey := "YOUR_API_KEY" // Replace with your Google Maps API key
-	// apiKey := "AIzaSyC2qJatOdSsOK1XY_TcedFOkABy297DOVw"
-	// apiKey := "AIzaSyDUnuzfv6Vib7TvH-CZUm0MjW0_LL-CjoY"
-	// apiKey := ""
-	// client, err := maps.NewClient(maps.WithAPIKey(apiKey))
-	// maps.
-	// if err != nil {
-	// 	log.Fatalf("Failed to create Google Maps client: %v", err)
-	// }
-
-	// // Create a LatLng object
-	// latlng := &maps.LatLng{
-	// 	Lat: latitude,
-	// 	Lng: longitude,
-	// }
-
-	// // Perform reverse geocoding
-	// reverseGeocodeRequest := &maps.GeocodingRequest{
-	// 	LatLng: latlng,
-	// }
-
-	// // Call the Geocode API
-	// reverseGeocodeResponse, err := client.ReverseGeocode(context.Background(), reverseGeocodeRequest)
-	// if err != nil {
-	// 	log.Fatalf("Failed to reverse geocode: %v", err)
-	// }
-
-	// // Print the formatted address
-	// if len(reverseGeocodeResponse) > 0 {
-	// 	fmt.Println("Formatted Address:", reverseGeocodeResponse[0].FormattedAddress)
-	// } else {
-	// 	fmt.Println("No results found")
-	// }
 
 	address, err := reverseGeocode(latitude, longitude)
 	if err != nil {
